31a9bbb81a0e99e0f6b7b10b657e4252c89ff093
xgrisha <xgrisha@mail.ru>
2021-05-10 20:29:44 +0400

    Change project groupId artifactId, module names.

---
+++ blank-project-impl/src/main/java/com/chuprynin/epam/rd/blank/project/impl/MainImpl.java
@@ -0,0 +1,10 @@
+package com.chuprynin.epam.rd.blank.project.impl;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MainImpl {
+    public static void main(String[] args) {
+        log.debug("Hello world!");
+    }
+}
